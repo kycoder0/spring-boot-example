baff69ef32a4df9511e052e4167349b798e6d8f1
Trevor Rice <trevor.rice260@gmail.com>
2021-09-02 15:06:56 -0400

    added resilience4j and a rate limiter on getPeople endpoint

--- src/main/java/com/trevdev/example/controller/PersonController.java
+++ src/main/java/com/trevdev/example/controller/PersonController.java
@@ -2,7 +2,11 @@ package com.trevdev.example.controller;
 
 import com.trevdev.example.dto.Person;
 import com.trevdev.example.service.PersonService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,10 +23,19 @@ public class PersonController {
 
     // GET localhost:8080/api/v1/person ->
     @GetMapping
-    public List<Person> getPeople() {
-       return this.personService.getAllPeople();
+    // rate limit this endpoint, config for rate limit is in application.yml
+    // fallback is used when this method is rate limited
+    @RateLimiter(name="getPeople", fallbackMethod = "getPeopleFallback")
+    public ResponseEntity<List<Person>> getPeople() {
+
+        List<Person> allPeople = this.personService.getAllPeople();
+        return new ResponseEntity<>(allPeople, null, HttpStatus.OK);
     }
 
+    // return the "too many requests" http status code
+    public ResponseEntity<List<Person>> getPeopleFallback(Throwable throwable) {
+        return new ResponseEntity<>(null, null, HttpStatus.TOO_MANY_REQUESTS);
+    }
 
     // POST localhost:8080/api/v1/person ->
     @PostMapping
