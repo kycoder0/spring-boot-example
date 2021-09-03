b67c03da20344a632bd6a4d0e6208a433714b5c4
Trevor Rice <trevor.rice260@gmail.com>
2021-09-03 10:08:09 -0400

    added a bulkhead annotation to getAllPeople

--- src/main/java/com/trevdev/example/controller/PersonController.java
+++ src/main/java/com/trevdev/example/controller/PersonController.java
@@ -2,7 +2,9 @@ package com.trevdev.example.controller;
 
 import com.trevdev.example.dto.Person;
 import com.trevdev.example.service.PersonService;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
@@ -26,8 +28,15 @@ public class PersonController {
     // rate limit this endpoint, config for rate limit is in application.yml
     // fallback is used when this method is rate limited
     @RateLimiter(name="getPeople", fallbackMethod = "getPeopleFallback")
+    // limit the amount of concurrent calls on this endpoint
+    @Bulkhead(name="getPeople",  fallbackMethod = "getPeopleFallbackBulkhead")
     public ResponseEntity<List<Person>> getPeople() {
+        try {
+            Thread.sleep(3000);
+        }
+        catch (Exception e) {
 
+        }
         List<Person> allPeople = this.personService.getAllPeople();
         return new ResponseEntity<>(allPeople, null, HttpStatus.OK);
     }
@@ -35,6 +44,10 @@ public class PersonController {
     // return the "too many requests" http status code
     public ResponseEntity<List<Person>> getPeopleFallback(Throwable throwable) {
         return new ResponseEntity<>(null, null, HttpStatus.TOO_MANY_REQUESTS);
+    }
+    // quests" http status code
+    public ResponseEntity<List<Person>> getPeopleFallbackBulkhead(Throwable throwable) {
+        return new ResponseEntity<>(null, null, HttpStatus.BANDWIDTH_LIMIT_EXCEEDED);
     }
 
     // POST localhost:8080/api/v1/person ->
