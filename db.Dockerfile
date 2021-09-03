c326db5d34dfc324501d6e23e8225b348ea2875e
Trevor Rice <trevor_rice@M-C02G19ZNQ05P.us.pjids.net>
2021-09-03 12:00:11 -0400

    removed dockerfiles and used docker-compose instead

--- db.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM postgres
-ENV POSTGRES_PASSWORD password
-ENV POSTGRES_DB spring
-COPY db/init.sql /docker-entrypoint-initdb.d/
--- start.sh
+++ start.sh
@@ -1,14 +1,2 @@
-docker build -t spring-pg-db -f db.Dockerfile .
-
-docker container stop spring-pg-container
-docker container rm spring-pg-container
-
-docker run -d --name spring-pg-container -p 5432:5432 spring-pg-db
-
-
-docker build -t spring-prom -f prom.Dockerfile .
-
-docker container stop spring-prom-container
-docker container rm spring-prom-container
-
-docker run -d --name spring-prom-container -p 9090:9090 spring-prom
+docker-compose down -v
+docker-compose up -d
